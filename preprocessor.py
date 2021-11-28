032fbbd77333e36e15f85c231da11cfbcc00b465
Fabian <fab.beringer@gmail.com>
2021-11-28 19:06:10 +0100

    implement bert preprocessor

--- preprocessor.py
+++ preprocessor.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 
+from torch.utils.data.dataloader import default_collate
+from transformers import BertTokenizer, BatchEncoding
+
 
 class Preprocessor(ABC):
     """Process an instance in the edge probing format and collate multiple into a batch.
@@ -20,9 +23,35 @@ class Preprocessor(ABC):
         :param data: the data to transform into a batch as returned by preprocess.
         :return: a batch or a tuple of batches
         """
-        pass
+        return default_collate(data)
+
+    def __call__(self, *args, **kwargs):
+        return self.preprocess(*args, **kwargs)
 
 
-class DummyPreprocessor(Preprocessor):
-    def preprocess(self, edge_probe):
-        return [ord(c) for c in edge_probe['text']], ['targets']
+class BERTPreprocessor(Preprocessor):
+    def __init__(self, tokenizer_name):
+        self.tokenizer = BertTokenizer.from_pretrained(tokenizer_name)
+
+    def preprocess(self, input_text):
+        """
+        :param input_text: the text to be fed into the subject model.
+        :return: the subject model input representation
+        """
+        tokens = self.tokenizer(input_text, truncation=True)
+        return tokens
+
+    def collate(self, data):
+        encodings, spans, labels = zip(*data)
+
+        batch_enc = BatchEncoding({'input_ids': [],
+                                   'token_type_ids': [],
+                                   'attention_mask': []})
+
+        for enc in encodings:
+            batch_enc['input_ids'].append(enc['input_ids'])
+            batch_enc['token_type_ids'].append(enc['token_type_ids'])
+            batch_enc['attention_mask'].append(enc['attention_mask'])
+
+        batch_enc = self.tokenizer.pad(batch_enc, return_tensors='pt')
+        return (batch_enc, spans), labels
