dcc20a2aad8cfe9d26876f0059d26176842bde31
Fabian <fab.beringer@gmail.com>
2021-11-28 18:30:50 +0100

    map labels to integer ids in dataset

--- datamodule/probing_datamodule.py
+++ datamodule/probing_datamodule.py
@@ -4,6 +4,7 @@ from hydra.utils import to_absolute_path
 from omegaconf import DictConfig
 from torch.utils.data import Dataset
 
+from datamodule import DatasetSplit
 from datamodule.default_datamodule import AbstractDefaultDataModule
 from preprocessor import Preprocessor
 
@@ -24,37 +25,53 @@ class ProbingDataModule(AbstractDefaultDataModule):
         self._dataset_conf = dataset
         self._preprocessor = preprocessor
 
+        self._label2id = self.read_labels(self._dataset_conf.label_file)
+
     @property
     def train_ds(self):
-        return JSONLDataset(self._dataset_conf.train_file, self._preprocessor)
+        return JSONLDataset(self._dataset_conf.train_file, self._label2id, self._preprocessor)
 
     @property
     def val_ds(self):
-        return JSONLDataset(self._dataset_conf.dev_file, self._preprocessor)
+        return JSONLDataset(self._dataset_conf.dev_file, self._label2id, self._preprocessor)
 
     @property
     def test_ds(self):
-        return JSONLDataset(self._dataset_conf.test_file, self._preprocessor)
+        return JSONLDataset(self._dataset_conf.test_file, self._label2id, self._preprocessor)
+
+    @staticmethod
+    def read_labels(label_file):
+        with open(to_absolute_path(label_file), 'r') as fp:
+            label2id = {x.rstrip(): i for i, x in enumerate(fp)}
+        return label2id
 
 
 class JSONLDataset(Dataset):
     """A dataset that reads dict instances from a jsonl file into memory, and applies preprocessing to each instance.
     """
 
-    def __init__(self, filepath, preprocessor: Preprocessor):
+    def __init__(self, filepath, label2id, preprocessor: Preprocessor):
         """
 
         :param filepath: path to the jsonl file to load instances from.
-        :param preprocessor: the preprocessors which is applied to each instance.
         """
         self._preprocessor = preprocessor
         with open(to_absolute_path(filepath), 'r') as fp:
-            self.x = tuple(json.loads(line) for line in fp)
+            self.instances = tuple(json.loads(line) for line in fp)
+
+        # replace label strings with integer ids for training
+        for instance in self.instances:
+            for target in instance['targets']:
+                label_str = target['label']
+                target['label'] = label2id[label_str]
 
     def __getitem__(self, index):
-        # TODO depending on ds size we might want to cache preprocessed instances in memory
-        x, y = self._preprocessor.preprocess(self.x[index])
-        return x, y
+        x = self.instances[index]
+
+        spans, labels = zip(*[(t['span1'], t['label']) for t in x['targets']])
+        subject_in = self._preprocessor(x['text'])
+
+        return subject_in, spans, labels
 
     def __len__(self):
-        return len(self.x)
+        return len(self.instances)
