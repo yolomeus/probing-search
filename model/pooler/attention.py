92d84ff55944c995abad0d8f5a11e632a5a337be
Fabian <fab.beringer@gmail.com>
2021-11-30 19:24:18 +0100

    implement attention pooling for pair targets

--- model/pooler/attention.py
+++ model/pooler/attention.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Module, Linear
+from torch.nn import Module, Linear, ModuleList
 
 
 class AttentionPooler(Module):
@@ -9,23 +9,45 @@ class AttentionPooler(Module):
     def __init__(self, input_dim, hidden_dim, layer_to_probe):
         super().__init__()
         self.layer_to_probe = layer_to_probe
-        self.in_projection = Linear(input_dim, hidden_dim)
-        self.attention_scorer = Linear(hidden_dim, 1, bias=False)
+        self.in_projections = ModuleList([Linear(input_dim, hidden_dim),
+                                          Linear(input_dim, hidden_dim)])
+        self.attention_scorers = ModuleList([Linear(hidden_dim, 1, bias=False),
+                                             Linear(hidden_dim, 1, bias=False)])
 
     def forward(self, hidden_states, target_spans):
         """
-
         :param hidden_states: the hidden states of the subject model with shape (N, L, D), i.e. batch size,
         sequence length and hidden dimension.
-        :param target_spans: a list of spans where each span is represented as triple: (i, a, b) with i being the
+        :param target_spans: a tensor of spans where each span is represented as triple: (i, a, b) with i being the
         span's position in the batch, and [a, b) the span interval along the sequence dimension.
-        :return:
+        :return: a tensor of pooled span embeddings of dimension `hidden_dim` or 2 * `hidden_dim` for pair tasks.
+        """
+
+        is_span_pair = len(target_spans) == 2 and isinstance(target_spans, list)
+        if is_span_pair:
+            pooled_span1 = self._single_span_pool(hidden_states, target_spans[0], 0)
+            pooled_span2 = self._single_span_pool(hidden_states, target_spans[1], 1)
+
+            return torch.cat([pooled_span1, pooled_span2], dim=1)
+
+        # pooling over a single span
+        return self._single_span_pool(hidden_states, target_spans, 0)
+
+    def _single_span_pool(self, hidden_states, target_spans, k):
+        """
+        :param hidden_states: the hidden states of the subject model with shape (N, L, D), i.e. batch size,
+        sequence length and hidden dimension.
+        :param target_spans: a tensor of spans where each span is represented as triple: (i, a, b) with i being the
+        span's position in the batch, and [a, b) the span interval along the sequence dimension.
+        :param k: one of two span specific parameter sets to use with `k` in [0, 1].
+        :return: a tensor of pooled span embeddings.
         """
         hidden_states = hidden_states[self.layer_to_probe]
         embed_spans = [hidden_states[i, a:b] for i, a, b in target_spans]
 
-        embed_spans = [self.in_projection(span) for span in embed_spans]
-        att_vectors = [self.attention_scorer(span).softmax(0)
+        # apply projections with parameters for span target k
+        embed_spans = [self.in_projections[k](span) for span in embed_spans]
+        att_vectors = [self.attention_scorers[k](span).softmax(0)
                        for span in embed_spans]
 
         pooled_spans = [att_vec.T @ embed_span
