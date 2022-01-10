dd6e9fe8d436176fa404c10588fe5f16adc5c481
Fabian <fab.beringer@gmail.com>
2022-01-10 20:17:07 +0100

    add MLP-2 from Hewitt and Liang

--- model/probing/mlp.py
+++ model/probing/mlp.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from torch.nn import Module, Sequential, Linear, Tanh, LayerNorm, Dropout
+from torch.nn import Module, Sequential, Linear, Tanh, LayerNorm, Dropout, ReLU
 
 
 class MLP(Module, ABC):
@@ -30,19 +30,32 @@ class MLP(Module, ABC):
 
 
 class TenneyMLP(MLP):
-    """The 2 layer MLP used by Tenney et al. in https://arxiv.org/pdf/1905.06316.pdf.
+    """The 2 layer MLP used by Tenney et al. in https://arxiv.org/abs/1905.06316.
 
     https://github.com/nyu-mll/jiant/blob/ead63af002e0f755c6418478ec3cabb4062a601e/jiant/modules/simple_modules.py#L49
     """
-
-    def __init__(self, input_dim, hidden_dim, output_dim, dropout, pair_targets):
-        super().__init__(input_dim, hidden_dim, output_dim, dropout, pair_targets)
 
     def _build_mlp(self):
         return Sequential(
             Linear(self.input_dim, self.hidden_dim),
             Tanh(),
             LayerNorm(self.hidden_dim),
+            Dropout(self.dropout),
+            Linear(self.hidden_dim, self.output_dim)
+        )
+
+
+class HewittMLP(MLP):
+    """MLP-2 from Hewitt and Liang: https://arxiv.org/abs/1909.03368.
+    """
+
+    def _build_mlp(self):
+        return Sequential(
+            Linear(self.input_dim, self.hidden_dim),
+            ReLU(),
+            Dropout(self.dropout),
+            Linear(self.hidden_dim, self.hidden_dim),
+            ReLU(),
             Dropout(self.dropout),
             Linear(self.hidden_dim, self.output_dim)
         )
