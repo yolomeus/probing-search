49bfacc078b49bb2485123f1e7ab10f40e63fbfc
Fabian <fab.beringer@gmail.com>
2022-01-12 14:55:21 +0100

    add bert with local checkpoint loading

--- model/subject/bert.py
+++ model/subject/bert.py
@@ -1,3 +1,4 @@
+import torch
 from torch.nn import Module
 from transformers import BertModel
 from transformers.utils import logging
@@ -21,3 +22,14 @@ class BERTPretrained(Module):
     def forward(self, inputs):
         outputs = self.bert.forward(**inputs, output_hidden_states=True)
         return outputs.hidden_states
+
+
+class BERTBaseFromCheckpoint(Module):
+    def __init__(self, ckpt_path, model_name):
+        super().__init__()
+        self.model_name = model_name
+        state_dict = torch.load(ckpt_path)
+        self.bert = BertModel.from_pretrained('bert-base-uncased', state_dict=state_dict)
+
+    def forward(self, inputs):
+        return self.bert.forward(**inputs, output_hidden_states=True).hidden_states
