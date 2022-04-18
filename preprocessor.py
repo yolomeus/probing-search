12a5b1c5481facca5d4c2fa04daa7dfef34d4e2e
Fabian <fab.beringer@gmail.com>
2022-04-18 21:34:45 +0200

    cast labels to tensor if not bucketizing

--- preprocessor.py
+++ preprocessor.py
@@ -84,6 +84,7 @@ class BERTPreprocessor(EdgeProbingPreprocessor):
         transformers.logging.set_verbosity_error()
         query, passage = input_text.split(' [SEP] ')
 
+        labels = torch.tensor(labels)
         if self.bucketize_labels:
             labels = self._bucketize_labels(labels)
 
